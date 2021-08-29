313a399cc3cafad7d8efc6aa26c7b43fb5705243
Joe McDonald <joemcdonald@riskgenius.com>
2021-08-29 11:20:55 -0500

    Initial Commit

---
+++ PowerCalculator/PowerCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerCalculatorApp.swift
+//  PowerCalculator
+//
+//  Created by Joe McDonald on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct PowerCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
